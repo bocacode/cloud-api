d6f04ffdf1297c7e5b3a0ab7f438608fb4a4eb7b
Todd Albert <todd@bocacode.com>
2022-10-18 17:19:28 -0400

    added patch

--- functions/src/users.js
+++ functions/src/users.js
@@ -18,3 +18,12 @@ export async function getAllUsers(req, res) {
   })
   res.send(users)
 }
+
+export async function updateUser(req, res) {
+  // const uid = req.params.uid
+  const { uid } = req.params
+  const db = dbConnect()
+  const doc = await db.collection('users').doc(uid).update(req.body)
+    .catch(err => res.status(500).send({ success: false, message: err }));
+  res.status(202).send({ success: true, message: 'User updated: ' + doc.id });
+}
