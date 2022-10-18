0802735b8944c578a7d20676c22f94e3a65c4b82
Todd Albert <todd@bocacode.com>
2022-10-18 16:44:18 -0400

    getAllUsers and createUser working

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 import functions from 'firebase-functions'
 import express from 'express'
+import { createUser, getAllUsers } from './src/users.js'
 
 const app = express()
+app.use(express.json())
 
-app.get('/test', (req, res) => res.send('Our first cloud API works, MF!'))
+app.get('/users', getAllUsers)
+app.post('/users', createUser)
 
 export const api = functions.https.onRequest(app)
---
+++ functions/src/users.js
@@ -0,0 +1,20 @@
+import { dbConnect } from "./dbConnect.js";
+
+export async function createUser(req, res) {
+  const db = dbConnect();
+  const doc = await db.collection('users').add(req.body)
+    .catch(err => res.status(500).send({ success: false, message: err }));
+  res.status(201).send({ success: true, message: 'User created: ' + doc.id });
+}
+
+export async function getAllUsers(req, res) {
+  const db = dbConnect();
+  const collection = await db.collection('users').get()
+    .catch(err => res.status(500).send({ success: false, message: err }));
+  const users = collection.docs.map(doc => {
+    let user = doc.data()
+    user.uid = doc.id
+    return user
+  })
+  res.send(users)
+}
